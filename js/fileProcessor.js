3ac75a6418cf7c803251571b806280a2dde361ef
Dhanavi Shah <DHANAVI.SHAH065@nmims.in>
2026-04-11 10:17:18 +0530

    Update fileProcessor.js

--- js/fileProcessor.js
+++ js/fileProcessor.js
@@ -130,7 +130,7 @@ async function extractPPTX(file) {
 // ===============================
 async function identifyTopics(text) {
 
-  const API_KEY = "AIzaSyDp1_iYxfKoCIXPgBfNkUAvgtU4dFqOfvA"; // 🔐 Put your real key here
+  const API_KEY = "AIzaSyCfSwwxsx1jZUAzIF4FLs-2AHU8CEmOYwI"; // 🔐 Put your real key here
 
   const response = await fetch(
     `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
